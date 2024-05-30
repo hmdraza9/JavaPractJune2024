c1a01eaff06b5dbbcf2f25d078fe0bf54a2d117c
hraza-clgx <hraza@corelogic.com>
2024-05-30 23:26:36 +0530

    Added create file by itself

--- src/main/java/convert/base64/toFile/Base64Decoder.java
+++ src/main/java/convert/base64/toFile/Base64Decoder.java
@@ -1,5 +1,6 @@
 package convert.base64.toFile;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -20,34 +21,36 @@ public class Base64Decoder {
 	}
 
 	public static void main(String[] args) throws IOException {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		String base64String = "";
-		
+
 		do {
 
 			// Prompt the user for the Base64 encoded string
 			System.out.println("Enter the Base64 encoded string:");
 			base64String = scanner.nextLine();
-			if(base64String==null||base64String.isEmpty()) {
+			if (base64String == null || base64String.isEmpty()) {
 				System.out.println("Base64 encoded string cannot be empty\n");
 				base64String = null;
 			}
-		}
-		while(base64String==null);
+		} while (base64String == null);
 
 		// Prompt the user for the output file path
 		System.out.println("Enter the output file extension:");
 		String outputFilePathExtension = scanner.nextLine();
 
 		String outputFileName = "";
+		String outputFolderName = "Output";
+
+		new File(outputFolderName).mkdirs();
 
 		if (outputFilePathExtension == null || outputFilePathExtension.isEmpty()) {
 			outputFilePathExtension = "pdf";
 		}
 
-		outputFileName = "TempFile_" + getTime() + "." + outputFilePathExtension;
+		outputFileName = outputFolderName + "/TempFile_" + getTime() + "." + outputFilePathExtension;
 
 		decodeBase64ToFile(base64String, outputFileName);
 
